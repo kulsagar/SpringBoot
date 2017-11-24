950afce64dd9bdc36ec82b95c10b8186e160687d
Kulkarni <sagar.a.kulkarni@capgemini.com>
2017-11-24 09:42:46 +0530

    App using Java based configuration

---
+++ 04.AppUsingJavaBasedConfiguration/src/main/java/demo/App.java
@@ -0,0 +1,43 @@
+package demo;
+
+import java.math.BigDecimal;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import demo.beans.Customer;
+import demo.service.WalletService;
+
+
+public class App{
+	public static void main( String[] args ){
+
+
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		WalletService service = ctx.getBean("service",WalletService.class);
+		Customer c = service.createAccount("Sagar", "9850276767", new BigDecimal(2000));
+
+
+		System.out.println("==============================================");
+		System.out.println("Create Account returned :: ");
+		System.out.println(c);
+		System.out.println();
+		System.out.println("==============================================");
+
+		System.out.println("Show balance returned :: ");
+		System.out.println(service.showBalance("9850276767"));
+		System.out.println();
+		System.out.println("==============================================");
+
+		System.out.println("Deposit returned :: ");
+		System.out.println(service.depositAmount("9850276767", new BigDecimal(1500)));
+		System.out.println();
+		System.out.println("==============================================");
+
+		System.out.println("Fund Transfer returned :: ");
+		service.createAccount("Renu", "9604744104",new BigDecimal(5000));
+		System.out.println(service.fundTransfer("9850276767", "9604744104", new BigDecimal(1500)));
+		System.out.println(service.showBalance("9604744104"));
+		System.out.println("==============================================");
+
+	}
+}
